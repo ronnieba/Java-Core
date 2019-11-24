543c8ed160247157a9203eaca883e760696836a3
jbt <jbt@307_21>
2019-11-24 15:27:18 +0200

    ok

--- Lesson 01/c/App10.java
+++ Lesson 01/c/App10.java
@@ -5,14 +5,20 @@ import java.util.Arrays;
 public class App10 {
 	public static void main(String[] args) {
 		int[] numbers = new int[10];
-		int sum = 0;
 		// Generates 10 Random Numbers in the range 1 -20
 		for (int i = 0; i < numbers.length; i++) {
 			numbers[i] = (int) (Math.random() * 100);
-			sum = sum + numbers[i];
 		} // end for loop
 		System.out.println("Numbers Generated: " + Arrays.toString(numbers));
-		System.out.println("Sum of Numbers: " + sum);
-		System.out.println("Average of Numbers: " + sum / numbers.length);
+
+		// Add 2 more elements
+		// 1.create new array of correct size
+		int[] temp = new int[numbers.length + 2];
+		// 2. copy elements
+		System.arraycopy(numbers, 0, temp, 0, numbers.length);
+		numbers = temp;
+		numbers[numbers.length - 2] = (int) (Math.random() * 100);
+		numbers[numbers.length - 1] = (int) (Math.random() * 100);
+		System.out.println("Numbers Generated: " + Arrays.toString(numbers));
 	}
 }
