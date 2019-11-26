2ef83230d976961f2858e6ebbca13a5cfafbb71f
jbt <jbt@307_21>
2019-11-26 10:45:30 +0200

    add Person

--- Lesson 02/src/c/persons/Person.java
+++ Lesson 02/src/c/persons/Person.java
@@ -2,4 +2,20 @@ package c.persons;
 
 public class Person {
 
+	int id;
+	String name;
+	int age;
+
+	void printName() {
+		System.out.println("my name is: " + this.name);
+	}
+
+	void printAge() {
+		System.out.println("my age is: " + age);
+	}
+
+	static void WhatIsAperson() {
+		System.out.println("a prson is a ");
+	}
+
 }
--- Lesson 02/src/c/persons/Test.java
+++ Lesson 02/src/c/persons/Test.java
@@ -2,4 +2,30 @@ package c.persons;
 
 public class Test {
 
+	public static void main(String[] args) {
+		Person p1 = new Person();
+		Person p2 = new Person();
+		Person p3 = new Person();
+
+		p1.name = "David";
+		p2.name = "Lea";
+		p1.age = 40;
+		p2.age = 30;
+
+		System.out.println(p1.name);
+		System.out.println(p1.age);
+		System.out.println(p2.name);
+		System.out.println(p1.age);
+		System.out.println(p3.name);
+		System.out.println(p3.age);
+
+		System.out.println("================");
+
+		p1.printName();
+		p2.printName();
+		p3.printName();
+
+		Person.WhatIsAperson();
+		p1.WhatIsAperson();
+	}
 }
