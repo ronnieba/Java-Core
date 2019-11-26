bd7d4d5ea1011bf47824d15ef391d13cb5445594
jbt <jbt@307_21>
2019-11-26 10:49:22 +0200

    a

--- Lesson 02/src/c/persons/Person.java
+++ Lesson 02/src/c/persons/Person.java
@@ -14,8 +14,9 @@ public class Person {
 		System.out.println("my age is: " + age);
 	}
 
-	static void WhatIsAperson() {
-		System.out.println("a prson is a ");
+	// class method
+	static void whatIsAperson() {
+		System.out.println("a person is an entity with id name and age");
 	}
 
 }
